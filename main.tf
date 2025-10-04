376ba0d5932617d5ede1b4926117ae1c655f31bd
Heldapari <nparimala@outlook.com>
2025-10-04 14:22:03 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "Parimala" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-052064a798f08f0d3"  
+  subnet_id = "subnet-"  
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
